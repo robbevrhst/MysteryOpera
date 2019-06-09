0b70a05f650339a3060f6abf6d4472a8b5610f24
Robbe Verhoest <robbe.verhoest@student.howest.be>
2019-06-09 20:46:04 +0200

    Initial Commit

---
+++ MysteryOpera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MysteryOpera
+//
+//  Created by Robbe Verhoest on 09/06/2019.
+//  Copyright © 2019 Robbe Verhoest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
